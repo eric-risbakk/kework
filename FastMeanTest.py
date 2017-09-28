f54688c0a3277ec68e7f82d2e233e6b010f1718a
Eric Risbakk <e.risbakk@student.maastrichtuniversity.nl>
2017-09-28 17:42:22 +0200

    Well, no errors, but now the program will not compile properly!

--- FastMeanTest.py
+++ FastMeanTest.py
@@ -2,15 +2,15 @@
 import pyopencl as cl
 import numpy as np
 
-# TODO: Lol, a function has only one output by definition.
+# Define
+TOL = 0.001  # Tolerance for floating point type comparisons
 
 # Experimental data.
 problems = 10  # Let a row correspond to a problem.
-data_points = 10
-inp = np.random.rand(problems, data_points).astype(np.float32)
-goal = np.empty(problems)
-out_mean = np.empty_like(goal)
-out_std = np.empty_like(goal)
+data_points = 5  # Elements in each row.
+inp = np.random.rand(problems, data_points).astype(np.float32)  # input.
+out_mean = np.empty(problems).astype(np.float32) # Output
+out_std = np.empty(problems).astype(np.float32)  # Output
 
 # The program. Finds the mean and standard deviance of the data from input.
 kernel_source = """
@@ -27,7 +27,7 @@ kernel_source = """
             float value;
 
             for (uint i = 0; i < length; ++i) {
-                value = input[gid+i];
+                value = input[i];
                 delta = value - mean;
                 mean += delta/(i+1);
                 variance += delta*(value-mean);
@@ -46,8 +46,10 @@ device = platform.get_devices()[0]
 
 context = cl.Context([device])
 
+print("Building program.")
 program = cl.Program(context, kernel_source).build()
-program.fast_mean_test.set_scalar_arg_dtypes([None, np.uint32, None, None])
+fmt = program.fast_mean_test
+fmt.set_scalar_arg_dtypes([None, np.uint32, None, None])
 
 queue = cl.CommandQueue(context)
 
@@ -57,11 +59,26 @@ inp_buf = cl.Buffer(context, mem_flags.READ_ONLY | mem_flags.COPY_HOST_PTR, host
 mean_buf = cl.Buffer(context, mem_flags.WRITE_ONLY, out_mean.nbytes)
 std_buf = cl.Buffer(context, mem_flags.WRITE_ONLY, out_std.nbytes)
 
-program.fast_mean_test(queue, (2, problems), None, inp_buf, data_points, mean_buf, std_buf)
+fmt(queue, inp.shape, None, inp_buf, data_points, mean_buf, std_buf)
 
 cl.enqueue_copy(queue, out_mean, mean_buf)
 cl.enqueue_copy(queue, out_std, std_buf)
 
-print(out_mean)
-print(out_std)
+print("Let us compare values!")
+print("------------------------------------------------------")
+means = np.mean(inp, axis=1)
+stds = np.std(inp, axis=1, ddof=1)
+correct = 0
+for i in range(problems):
+    diff_m = out_mean[i] - means[i]
+    diff_s = out_std[i] - stds[i]
+    if (diff_m**2 < TOL**2) & (diff_s**2 < TOL**2):
+        correct += 1
+    else:
+        print("Not alike! Element:", i)
+        print("\tdiff_m", diff_m, "\tdiff_s", diff_s)
+        print("\tmeans/out_mean (", means[i], "/", out_mean[i], ")")
+        print("\tstds/out_std (", stds[i], "/", out_std[i])
+
+print("Correct:", correct, "out of ", problems, ".")
 
