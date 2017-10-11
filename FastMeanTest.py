8b61fc990d094f6adc66f69934202a115dac6dea
Eric Risbakk <e.risbakk@student.maastrichtuniversity.nl>
2017-10-11 14:48:20 +0200

    I believe I have successfully called the function, that does the work, from another function!

--- FastMeanTest.py
+++ FastMeanTest.py
@@ -40,6 +40,46 @@ kernel_source = """
         }    
 """
 
+kernel_source_fun = """
+    // Calculates mean and variance with index already being set in input.
+    // The meanOut and stdOut using get_global_id() to find their spot has been
+    // abstracted out Now it accommodates (or so I think) the memory address sent.
+    void mean_fancy_indexing(
+        __global float* input,
+        uint length,
+        __global float* meanOut,
+        __global float* stdOut) 
+    {
+        float mean = 0;
+        float variance = 0;
+        float delta;
+        float value;
+
+        for (uint i = 0; i < length; ++i) {
+            value = input[i];
+            delta = value - mean;
+            mean += delta/(i+1);
+            variance += delta*(value-mean);
+        }
+
+        variance /= (length - 1);
+
+        *meanOut = mean;
+        *stdOut = sqrt(variance);
+    }
+
+    __kernel void fast_mean_test(
+        __global float* input,
+        uint length,
+        __global float* meanOut,
+        __global float* stdOut)
+        {
+            int gid = get_global_id(0);
+            global float* inp_loc = input + gid*length;
+            mean_fancy_indexing(inp_loc, length, &meanOut[gid], &stdOut[gid]);
+        }
+"""
+
 platform = cl.get_platforms()[0]
 
 device = platform.get_devices()[0]
@@ -47,7 +87,7 @@ device = platform.get_devices()[0]
 context = cl.Context([device])
 
 print("Building program.")
-program = cl.Program(context, kernel_source).build()
+program = cl.Program(context, kernel_source_fun).build()
 fmt = program.fast_mean_test
 fmt.set_scalar_arg_dtypes([None, np.uint32, None, None])
 
@@ -63,6 +103,7 @@ std_buf = cl.Buffer(context, mem_flags.WRITE_ONLY | mem_flags.USE_HOST_PTR, host
 
 fmt(queue, inp.shape, None, inp_buf, data_points, mean_buf, std_buf)
 
+# Replacing the enqueue_copy with enqueue_map_buffer.
 # cl.enqueue_copy(queue, out_mean, mean_buf)
 # cl.enqueue_copy(queue, out_std, std_buf)
 # enqueue_map_buffer(queue, buf, flags, offset, shape, dtype, order="C", strides=None, wait_for=None, is_blocking=True)
